48f2a15c326b593fe2b919414247eda7ca4bc06e
Ngozi <hexwhyzed01@gmail.com>
2026-04-16 20:47:44 +0100

    reconstruct database schema

--- db.js
+++ db.js
@@ -3,10 +3,18 @@ const db = require("better-sqlite3")("myapp.db");
 db.pragma("journal_mode = WAL");
 
 db.exec(`
-    CREATE TABLE IF NOT EXISTS profiles(
+    CREATE TABLE IF NOT EXISTS profiles (
     id TEXT PRIMARY KEY,
-    name TEXT NOT NULL
-    )
+    name TEXT UNIQUE NOT NULL,
+    gender TEXT,
+    gender_probability REAL,
+    sample_size INTEGER,
+    age INTEGER,
+    age_group TEXT,
+    country_id TEXT,
+    country_probability REAL,
+    created_at TEXT
+  )
     `);
 
 module.exports = db;
