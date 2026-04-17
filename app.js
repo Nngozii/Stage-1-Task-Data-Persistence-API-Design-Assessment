6a48747c290172313cf893cca295b296f0707573
Ngozi <hexwhyzed01@gmail.com>
2026-04-17 13:33:59 +0100

    add ssl to database pool

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 
 // ─── Database Setup ───────────────────────────────────────────────────────────
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 pool.query(`
   CREATE TABLE IF NOT EXISTS profiles (
